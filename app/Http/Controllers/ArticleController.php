2942f58943fcbcdd2954f51ba017191ab2ad1f1a
EW <cshrik3@gmail.com>
2017-04-06 01:15:07 +0800

    testing image

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -9,7 +9,17 @@ class ArticleController extends Controller
     //
     public function index(){
         $result = \App\Article::all();
+        foreach ($result as $a) {
+            $a['front_image']=url('images/x.jpg');
+        }
         return $result;
     }
+
+    public function show($id){
+        $result = \App\Article::find($id);
+        $result['front_image']=url('images/x.jpg');
+        return $result;
+    }
+
 
 }

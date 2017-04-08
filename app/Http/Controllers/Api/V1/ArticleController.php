1d5209fa5f7632848cc50708c06e65a0f37c93fe
EW <cshrik3@gmail.com>
2017-04-09 02:01:30 +0800

    fake implement of some relationships

--- app/Http/Controllers/Api/V1/ArticleController.php
+++ app/Http/Controllers/Api/V1/ArticleController.php
@@ -13,6 +13,8 @@ class ArticleController extends Controller
         $result = \App\Article::all();
         foreach ($result as $a) {
             $a['front_image']=url('images/x.jpg');
+            $a['icon_image'] = url('images/unnamed.png');
+            $a['likes'] = rand(0,2333);
         }
         return $result;
     }
@@ -20,13 +22,14 @@ class ArticleController extends Controller
     public function show($id)
     {
         $result = \App\Article::find($id);
+        // 这里是一个伪实现，模型关系未完成
         if ($result) {
             $result['front_image']=url('images/x.jpg');
+            $result['icon_image'] = url('images/unnamed.png');
+            $result['likes'] = rand(0,2333);
         }
 
         //str_replace("\\/" , "/" , json_encode($array_name));
-        $test1 = str_replace("\\/", "/", json_encode($result));
-        $test2 = json_encode($result);
-        return $test1;
+        return $result;
     }
 }
--- app/Http/Controllers/Api/V1/CommunityController.php
+++ app/Http/Controllers/Api/V1/CommunityController.php
@@ -9,11 +9,17 @@ class CommunityController extends Controller
     //
     public function show($id){
         $r1 = \App\Community::find($id);
+        if($r1){
+            $r1['icon'] = url('/images/unnamed.png');
+        }
         return $r1;
     }
 
     public function index(){
         $result = \App\Community::all();
+        foreach($result as $a){
+            $a['icon'] = url('/images/unnamed.png');
+        }
         return $result;
 
     }
